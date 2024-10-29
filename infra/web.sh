a65f3930dcb70f0a5d845b4a998b1f1f0396f049
oriafo <abrahamibharunujele@gmail.com>
2024-10-29 13:38:31 +0100

    update: change to a temporary directory before using wget to downloa the site

--- infra/web.sh
+++ infra/web.sh
@@ -10,7 +10,7 @@ sudo systemctl status nginx
 # cd /tmp 
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
 # if [ -f "2137_barista_cafe.zip" ]; then
-sudo unzip 2137_barista_cafe.zip -d /var/www/html
+sudo unzip -o 2137_barista_cafe.zip -d /var/www/html
 sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
 sudo nginx -s reload
 sudo systemctl restart nginx
