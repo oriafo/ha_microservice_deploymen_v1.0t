5f6615bc0ae763ae2abd6009240339f5b1ad91ed
oriafo <abrahamibharunujele@gmail.com>
2024-10-29 13:24:48 +0100

    update: change to a temporary directory before using wget to downloa the site

--- infra/web.sh
+++ infra/web.sh
@@ -7,15 +7,15 @@ sudo ufw status
 sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo systemctl status nginx
-cd /tmp 
+# cd /tmp 
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
-if [ -f "2137_barista_cafe.zip" ]; then
-    sudo unzip 2137_barista_cafe.zip -d /var/www/html
-    sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
-    sudo nginx -s reload
-    sudo systemctl restart nginx
-else
-    echo "Something went wrong while downloading the file"
-fi
+# if [ -f "2137_barista_cafe.zip" ]; then
+sudo unzip 2137_barista_cafe.zip -d /var/www/html
+sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
+sudo nginx -s reload
+sudo systemctl restart nginx
+# else
+    # echo "Something went wrong while downloading the file"
+# fi
 
 
