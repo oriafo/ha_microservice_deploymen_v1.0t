a9f32b86949eaeab2cfa471a108a9f3b5551079b
oriafo <abrahamibharunujele@gmail.com>
2024-10-30 18:13:38 +0100

    style: deleted commented lines that will not be needed any more

--- infra/instance.tf
+++ infra/instance.tf
@@ -20,63 +20,29 @@ provider "aws" {
 }
 
 
-# resource "aws_key_pair" "pub_key" {
-#   key_name = "key"  # Replace with the name of your key pair
-#   public_key = var.aws_public_key
-# }
-
-# Create a security group for instances
-# resource "aws_security_group" "instances" {
-#   name        = "instances-sg"
-#   description = "Allow HTTP inbound traffic and all outbound traffic"
-#   vpc_id      = data.aws_vpc.default.id
-
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
-#   }
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["${data.http.public_ip.body}/32"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-#   }
-# }
-
 resource "aws_instance" "provisioner_machine" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = var.instance_type
   key_name      = data.aws_key_pair.my_key.key_name
-  #key_name      = var.aws_public_key
   vpc_security_group_ids = [ data.aws_security_group.default.id ]
   tags = {
-    Name = "provisioner_machine"
+    Name = "provisioner_machine-${ github.head_ref }"
   }
   provisioner "file" {
-    source      = "web.sh"                # Local file path
-    destination = "/home/ubuntu/web.sh" # Destination on the instance
+    source      = "web.sh"                                      # Local file path
+    destination = "/home/ubuntu/web.sh"                         # Destination on the instance
     connection {
       type        = "ssh"
       host        = self.public_ip
       user        = "ubuntu"
-      private_key = var.aws_private_key    # Path to your SSH key
+      private_key = var.aws_private_key    
     }
   }
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /home/ubuntu/web.sh", # Make the file executable
-      "/home/ubuntu/web.sh",         # Execute the file
+      "chmod +x /home/ubuntu/web.sh",                            # Make the file executable
+      "/home/ubuntu/web.sh",                                     # Execute the file
     ]
 
     # Connect to the instance
@@ -84,7 +50,7 @@ resource "aws_instance" "provisioner_machine" {
       type        = "ssh"
       host        = self.public_ip
       user        = "ubuntu"
-      private_key = var.aws_private_key     # Path to your SSH key
+      private_key = var.aws_private_key     
     }
   }
 }
