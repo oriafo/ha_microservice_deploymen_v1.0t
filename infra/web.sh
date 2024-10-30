8302e6a05b8c2dbeb2d070c977b1a4d649851a99
oriafo <abrahamibharunujele@gmail.com>
2024-10-30 11:49:15 +0100

    update: add sudo priviledge to the cd command

--- infra/web.sh
+++ infra/web.sh
@@ -13,7 +13,7 @@ cd /tmp
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
 # if [ -f "2137_barista_cafe.zip" ]; then
 sudo mkdir -p /var/www/html
-sudo unzip -o 2137_barista_cafe.zip /var/www/html
+sudo unzip 2137_barista_cafe.zip -d /var/www/html
 sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
 sudo nginx -s reload
 sudo systemctl restart nginx
