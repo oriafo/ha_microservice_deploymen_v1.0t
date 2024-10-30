9ad5bc2862a55a94903c0ff59527ad8dc794e647
oriafo <abrahamibharunujele@gmail.com>
2024-10-30 18:11:57 +0100

    update: added a local variable that help me select between both environments private keys

--- infra/data.tf
+++ infra/data.tf
@@ -14,9 +14,20 @@
 #   owners = ["099720109477"] # Canonicall
 # }
 
-data "aws_key_pair" "my_key" {
-  key_name = "bastonHost-key"           # Replace with the name of your key pair
+# Define the SSH key pair names based on the environment
+locals {
+  ssh_key_name = var.environment_name == "dev" ? "bastonHost-key" : "bastonHost-key-prod"
 }
+
+# Data block to fetch the SSH key pair based on the environment
+data "aws_key_pair" "my_key" {
+  key_name = local.ssh_key_name
+}
+
+
+# data "aws_key_pair" "my_key" {
+#   key_name = "bastonHost-key"           
+# }
 
 data "aws_vpc" "default" {
   default = true
@@ -33,3 +44,8 @@ data "aws_security_group" "default" {
     values = ["default"]  # The name of the default security group is usually "default"
   }
 }
+
+
+
+#the keys shold be chossen based on ref
+# and the resources should have tags based on ref too.
